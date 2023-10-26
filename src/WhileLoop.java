cfa082d765367fa17c1fdc71ab1bfe7c5889d1b9
Gahasa Purba <gahasa.purba@ist.id>
2023-10-26 15:54:30 +0700

    21 - 25

---
+++ src/WhileLoop.java
@@ -0,0 +1,14 @@
+public class WhileLoop {
+
+    public static void main(String[] args) {
+
+        var counter = 1;
+
+        while (counter <= 10){
+            System.out.println("Perulangan " + counter);
+            counter++;
+        }
+
+    }
+
+}
