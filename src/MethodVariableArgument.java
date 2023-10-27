c27c4f5045b21e2f553daeb611178ca0dfccea24
Gahasa Purba <gahasa.purba@ist.id>
2023-10-27 15:54:26 +0700

    26 - 30

---
+++ src/MethodVariableArgument.java
@@ -0,0 +1,26 @@
+public class MethodVariableArgument {
+
+    public static void main(String[] args) {
+
+        int[] values = {80, 50, 50, 50, 80};
+        sayCongrats("Eko", values);
+
+        sayCongrats("Budi", 80, 90, 76, 80);
+
+    }
+
+    static void sayCongrats(String name, int... values){
+        var total = 0;
+        for (var value : values){
+            total += value;
+        }
+        var finalValues = total / values.length;
+
+        if (finalValues >= 75){
+            System.out.println("Selamat " + name + ", Anda Lulus");
+        } else {
+            System.out.println("Maaf " + name + ", Anda Tidak Lulus");
+        }
+    }
+
+}
