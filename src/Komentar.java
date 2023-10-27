be563fe237cb86b4b554fa0e2b8bb4165be9ea59
Gahasa Purba <gahasa.purba@ist.id>
2023-10-27 17:22:40 +0700

    31 - 33

---
+++ src/Komentar.java
@@ -0,0 +1,27 @@
+public class Komentar {
+
+    public static void main(String[] args) {
+
+        System.out.println(sum(10, 10));
+
+    }
+
+    /**
+     * Method untuk menjumlahkan value1 dan value2
+     * @param value1
+     * @param value2
+     * @return
+     */
+
+    /*
+    Komentar
+    Multi
+    Line
+     */
+
+    static int sum(int value1, int value2) {
+        // jumlahkan value1 dan value2
+        return value1 + value2;
+    }
+
+}
---
+++ src/Scope.java
@@ -0,0 +1,21 @@
+public class Scope {
+
+    public static void main(String[] args) {
+
+        sayHello("Eko");
+        sayHello("");
+
+    }
+
+    static void sayHello(String name){
+        String hello = "Hello " + name;
+
+        if (!name.isBlank()){
+            String hi = "Hi " + name;
+            System.out.println(hi);
+        }
+
+        System.out.println(hello);
+    }
+
+}
